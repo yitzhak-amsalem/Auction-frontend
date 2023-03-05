f39edb95964c2d6fdc36b53f86509b8bc835d5b0
Yitzhak Amsalem <yitzhak@gmail.com>
2023-03-05 02:18:18 +0200

    add content for product page and my offers page

--- src/pages/MyOffers.js
+++ src/pages/MyOffers.js
@@ -1,31 +1,65 @@
 import React, {useState, useEffect} from "react";
 import Cookies from "js-cookie";
-import {getUser} from "../services/GetUser";
 import {useNavigate} from "react-router-dom";
 import {getMyOffers} from "../services/GetMyOffers";
+import "../css/Table.css"
 
 
 export default function MyOffers() {
     const [myOffers, setMyOffers] = useState([]);
     const navigate = useNavigate();
+    const [token, setToken] = useState("5D96A0ED166A05B49DA80A031752FE80")
 
-    const [user, setUser] = useState({});
+
     useEffect(() => {
-        const token = Cookies.get("token");
+        //setToken(Cookies.get("token"));
         if (token === undefined) {
             navigate("../login");
         } else {
-            getUser(token, (response) => {
-                setUser(response.data.user);
-            })
             getMyOffers(token, (response) => {
                 setMyOffers(response.data)
             })
         }
     },[])
 
+    const goToProduct = (productID) => {
+        navigate(`/product/${productID}`)
+    }
+
     return (
-        <div className={"my-Offers-table"}>
+        <div id={"my-Offers-table"}>
+            {
+                myOffers.length > 0 &&
+                <table>
+                    <thead>
+                    <tr id={"table-row-header"}>
+                        <th className={"border-header"}>product</th>
+                        <th className={"border-header"}>amount</th>
+                        <th className={"border-header"}>open</th>
+                        <th className={"border-header"}>win</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        myOffers.map((offer, i) => {
+                            return (
+                                <tr style={{cursor: "pointer"}} onClick={() => goToProduct(offer.productID)} key={i}>
+                                    <td>{offer.productName}</td>
+                                    <td>{offer.amountOffer}</td>
+                                    <td>{offer.isOpen ? "open" : "close"}</td>
+                                    {
+                                        offer.isOpen ?
+                                            <td>-</td>
+                                            :
+                                            <td>{offer.isWin ? "win!" : "maybe next time"}</td>
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
+            }
 
         </div>
     );
