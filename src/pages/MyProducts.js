d9d4c75cfee28110356a3c08dd048ab2f8e3aa63
Elad shapiro <eladshapiro@gmail.com>
2023-03-08 19:05:04 +0200

    my products page

--- src/pages/MyProducts.js
+++ src/pages/MyProducts.js
@@ -1,23 +1,40 @@
 import React, {useState, useEffect} from "react";
-import DrawProduct from "../components/DrawProduct";
+import Cookies from "js-cookie";
+import {getUser} from "../services/GetUser";
+import {useNavigate} from "react-router-dom";
+import {getMyProducts} from "../services/GetMyProducts";
 
 
 export default function MyProducts() {
 
     const [MyProducts, setMyProducts] = useState([]);
+    const navigate = useNavigate();
 
-    /*useEffect(() => {
-        getLeagueTableLive((response) => {
-            const updatedTable = response.data;
-            setLeagueTableLive(updatedTable);
-        })
-    }, );*/
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (token === undefined) {
+            navigate("../login");
+        } else {
+            getUser(token, (response) => {
+                setUser(response.data.user);
+            })
+            getMyProducts(token, (response) => {
+                setMyProducts(response.data)
+            })
+        }
+    },[])
 
     return (
-        <div className={"My-Products"}>
-            <DrawProduct table={MyProducts}/>
+        <div className={"my-products-table"}>
+
         </div>
     );
 
-
 }
+
+
+
+
+
+
