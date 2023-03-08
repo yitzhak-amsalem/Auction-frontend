08c376f12d3a980519d6ea0bb6534d25aa886230
Elad shapiro <eladshapiro@gmail.com>
2023-03-08 20:01:22 +0200

    My products page as it should be..

--- src/pages/MyProducts.js
+++ src/pages/MyProducts.js
@@ -1,41 +1,62 @@
 import React, {useState, useEffect} from "react";
 import Cookies from "js-cookie";
-import DrawProduct from "../components/DrawProduct";
-import {getProduct} from "../services/GetMyProducts";
 import {useNavigate} from "react-router-dom";
+import {getMyProducts} from "../services/GetMyProducts";
 
 
 export default function MyProducts() {
 
     const [MyProducts, setMyProducts] = useState([]);
+    const [token, setToken] = useState("")
     const navigate = useNavigate();
 
-    useEffect(() => {
-        getProduct((response) => {
-            const updatedTable = response.data;
-            setMyProducts(updatedTable);
-        })
-    }, );
+
     useEffect(() => {
         const token = Cookies.get("token");
         if (token === undefined) {
             navigate("../login");
         } else {
-            getProduct((response) => {
-                const updatedTable = response.data;
-                setMyProducts(updatedTable);
+            getMyProducts(token, (response) => {
+                setMyProducts(response.data)
             })
         }
-    },[])
+    }, [])
     const goToProduct = (productID) => {
         navigate(`/product/${productID}`)
+
+
+        return (
+            <div id={"my-Offers-table"}>
+                {
+                    MyProducts.length > 0 &&
+                    <table>
+                        <thead>
+                        <tr id={"table-row-header"}>
+                            <th className={"border-header"}>product Name</th>
+                            <th className={"border-header"}>max Amount</th>
+                            <th className={"border-header"}>open</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            MyProducts.map((product, i) => {
+                                return (
+                                    <tr style={{cursor: "pointer"}} onClick={() => goToProduct(product.productID)}
+                                        key={i}>
+                                        <td>{product.productName}</td>
+                                        <td>{product.maxAmount}</td>
+                                        <td>{product.isOpen ? "open" : "close"}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                }
+
+            </div>
+        );
+
     }
-
-    return (
-        <div className={"My-Products"}>
-
-        </div>
-    );
-
 
 }
